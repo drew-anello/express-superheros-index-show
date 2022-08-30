84c5f7ca1dc90374e1b20afd22790bf29b45f88c
Drew Anello <drew.anello@gmail.com>
2022-08-30 11:18:36 -0400

    indexOfSuperheos working can sort by number 0-2

--- server.js
+++ server.js
@@ -2,10 +2,31 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const superHeros = ['Superman', 'Wonder Woman', 'Black Panther']
+const superHeros = [
+    {
+        name: 'Superman',
+        powers: ['flight', 'invulnerability', 'x-ray vision']
 
-app.get('/superheros/', (req,res) => {
+    },
+    {
+        name: 'Wonder Woman',
+        powers: 'super strength'
+    },
+    {
+        name: 'Black Panther',
+        powers: 'being dope'
+    }
+]
+
+const villians = [
+    
+]
+
+app.get('/superheros/', (req, res) => {
     res.send(superHeros)
+})
+app.get('/superheros/:indexOfSuperHeros', (req, res) => {
+    res.send(superHeros[req.params.indexOfSuperHeros])
 })
 
 app.listen(port, () => {
